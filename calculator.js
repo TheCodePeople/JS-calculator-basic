f90b8187231954e8ef89cf11dbe5fde54988bc6e
3lawi <alii.3mmar@gmail.com>
2023-07-30 15:47:52 +0300

    Complete UI design

--- calculator.js
+++ calculator.js
@@ -1,43 +1,92 @@
-function add(num1, num2) {
-    return num1 + num2;
-}
-function subtract(num1, num2) {
-    return num1 - num2;
-}
-function multiply(num1, num2){
-    return num1 * num2;
-}
-function divide(num1, num2){
-    return num1 / num2;
-}
+let currentInput = '';
+let previousInput = '';
+let currentOperator = '';
+let resultShown = false;
 
-let num1 = prompt("Enter the first number");
-let operator = prompt("Enter the operation (+, -, *, or /)");
-let num2 = prompt("Enter the second number");
+function handleButtonClick(value) {
+    if (resultShown) {
+        // If the result is shown and a new number/operator is clicked, start a new calculation
+        currentInput = value;
+        resultShown = false;
+    } else {
+        currentInput += value;
+    }
 
-num1 = parseInt(num1);
-num2 = parseInt(num2);
-
-let result;
-
-if (operator === "+") {
-    result = add(num1, num2);
-}
-else if (operator === "-") {
-    result = subtract(num1, num2);
-}
-else if (operator === "*") {
-    result = multiply(num1, num2);
-}
-else if (operator === "/") {
-    result = divide(num1, num2);
-}
-else {
-    alert("Invalid operator");
+    // Update the result display with the complete expression
+    updateResult();
 }
 
-alert(result);
+function updateResult() {
+    // Check if both previousInput and currentOperator have values before updating the result
+    if (previousInput !== '' && currentOperator !== '') {
+        document.getElementById('result').innerText = previousInput + ' ' + currentOperator + ' ' + currentInput;
+    } else {
+        document.getElementById('result').innerText = currentInput;
+    }
+}
 
+function calculateResult() {
+    const num1 = parseFloat(previousInput);
+    const num2 = parseFloat(currentInput);
 
+    switch (currentOperator) {
+        case '+':
+            currentInput = (num1 + num2).toString();
+            break;
+        case '-':
+            currentInput = (num1 - num2).toString();
+            break;
+        case '*':
+            currentInput = (num1 * num2).toString();
+            break;
+        case '/':
+            currentInput = (num1 / num2).toString();
+            break;
+    }
 
+    previousInput = '';
+    currentOperator = '';
+    resultShown = true;
+    updateResult();
+}
 
+function clearResult() {
+    currentInput = '';
+    previousInput = '';
+    currentOperator = '';
+    resultShown = false;
+    updateResult();
+}
+
+function handleOperator(operator) {
+    if (currentInput !== '') {
+        if (previousInput !== '') {
+            calculateResult();
+        } else {
+            previousInput = currentInput;
+        }
+        currentOperator = operator;
+        currentInput = '';
+    }
+
+    // Update the result display with the complete expression
+    updateResult();
+}
+
+function handleEqual() {
+    if (currentOperator !== '' && previousInput !== '' && currentInput !== '') {
+        calculateResult();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const equalButton = document.getElementById('equal');
+    equalButton.addEventListener('click', handleEqual);
+
+    const operatorButtons = document.querySelectorAll('.operator');
+    operatorButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            handleOperator(button.innerText);
+        });
+    });
+});
